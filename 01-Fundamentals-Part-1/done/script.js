2022822e8e3d6a99adf1d37c793ac2ec7d247146
Pravash Karki <84731929+pravashkarki@users.noreply.github.com>
2023-01-02 15:56:26 +0000

    naming conventions.

--- 01-Fundamentals-Part-1/done/script.js
+++ 01-Fundamentals-Part-1/done/script.js
@@ -4,7 +4,12 @@ console.log(12 + 36 + 45 - 10);
 console.log("Pravash Karki");
 console.log(23);
 
-let firstName = "Sunita";
+let firstName = "Sunita"; // Camel Case firstName
+let first = "Pravash";
+let firstPersonName = "Prasuv";
+let first_person_name = "Pranav";
+
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+console.log(first);
+console.log(firstPersonName);
+console.log(firstPersonName);
