37e5233b0d85d137f638424a30f793e7f392dcf0
Pravash Karki <84731929+pravashkarki@users.noreply.github.com>
2023-01-02 16:11:28 +0000

    Task: Values and Variables

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -1,13 +1,13 @@
 let js = "amazing";
 console.log(12 + 36 + 45 - 10);
 
-console.log("Pravash Karki");
+console.log('Pravash Karki');
 console.log(23);
 
-let firstName = "Sunita"; // Camel Case firstName
-let first = "Pravash";
-let firstPersonName = "Prasuv";
-let first_person_name = "Pranav";
+let firstName = 'Sunita'; // Camel Case firstName
+let first = 'Pravash';
+let firstPersonName = 'Prasuv';
+let first_person_name = 'Pranav';
 
 console.log(firstName);
 console.log(first);
---
+++ 01-Fundamentals-Part-1/task/script.js
@@ -0,0 +1,7 @@
+let country     = 'Nepal';
+let continent   = 'Asia';
+let population  = '30,505,178';
+
+console.log(country);
+console.log(continent);
+console.log(population);
