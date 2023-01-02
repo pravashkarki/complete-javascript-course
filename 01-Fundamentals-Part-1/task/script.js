a86f40f35ac6f29f1149cf234978aa7dd353d211
Pravash Karki <84731929+pravashkarki@users.noreply.github.com>
2023-01-02 16:13:11 +0000

    add: question on task one

--- 01-Fundamentals-Part-1/task/script.js
+++ 01-Fundamentals-Part-1/task/script.js
@@ -1,3 +1,13 @@
+/**
+
+Values and Variables
+
+1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+
+2. Log their values to the console
+
+**/
+
 let country     = 'Nepal';
 let continent   = 'Asia';
 let population  = '30,505,178';
