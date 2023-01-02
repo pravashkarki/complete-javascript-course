0ad50488c04085c57cfb9438239fdcf868f7b58d
Pravash Karki <84731929+pravashkarki@users.noreply.github.com>
2023-01-02 15:53:29 +0000

    creating var

--- 01-Fundamentals-Part-1/done/script.js
+++ 01-Fundamentals-Part-1/done/script.js
@@ -1,2 +1,10 @@
 let js = "amazing";
 console.log(12 + 36 + 45 - 10);
+
+console.log("Pravash Karki");
+console.log(23);
+
+let firstName = "Prasuv";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
